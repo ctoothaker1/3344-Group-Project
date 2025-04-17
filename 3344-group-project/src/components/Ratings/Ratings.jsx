7f23848e22340a46692eb199a8a56bc80eff3055
ianxshort <tut42883@temple.edu>
2025-04-16 21:39:16 -0400

    creating Ratings page/finished context

---
+++ 3344-group-project/src/components/Ratings/Ratings.jsx
@@ -0,0 +1,17 @@
+import { RatingsContext } from "../../components/Ratings/RatingsContext.jsx";
+import { FavoritesContext } from "../useContext/useContext";
+
+
+function Ratings({rating})
+{
+
+const {ratings,setRating}=useContext(RatingsContext);
+
+
+    return(
+
+
+
+    )
+}
+export default Ratings;
